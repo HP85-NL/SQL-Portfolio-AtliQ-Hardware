dd01770531a628bfb8d0bcc0b37c2dfc78bcfd7a
Patel_delivers <harshil.patel150@gmail.com>
2026-02-21 17:48:48 +0100

    Add files via upload

---
+++ Ad_hocs_Requirements/Req_5.sql
@@ -0,0 +1,16 @@
+# Get the products that have the highest and lowest manufacturing cost 
+# The final output contains these field ....
+# product_code and product manufacturing_cost
+
+
+
+SELECT F.product_code, P.product, F.manufacturing_cost 
+FROM fact_manufacturing_cost F JOIN dim_product P
+ON F.product_code = P.product_code
+WHERE manufacturing_cost
+IN (
+	SELECT MAX(manufacturing_cost) FROM fact_manufacturing_cost
+    UNION
+    SELECT MIN(manufacturing_cost) FROM fact_manufacturing_cost
+    ) 
+ORDER BY manufacturing_cost DESC ;
