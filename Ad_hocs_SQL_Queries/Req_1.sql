3018ef46cfdd122aa0155f0e18e2640473cd69ff
Patel_delivers <harshil.patel150@gmail.com>
2026-02-21 17:50:23 +0100

    Add files via upload

---
+++ Ad_hocs_SQL_Queries/Req_1.sql
@@ -0,0 +1,7 @@
+-- Provide the list of markets in which customer "AtliQ Exclusive" operates its business in the APAC region -- 
+
+SELECT market 
+	   FROM dim_customer 
+	   WHERE customer = 'Atliq Exclusive' AND region = 'APAC'
+       GROUP BY market
+       ORDER BY market ;
